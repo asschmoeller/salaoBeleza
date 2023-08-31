81947d2c005fb4751708ea5a0313a605a15e3793
HELEN CRISTINA DA SILVA <helen.silva@univille.br>
2023-08-31 20:09:28 -0300

    Produto e Fornecedor

--- src/main/java/br/edu/univille/salaobeleza/entity/Cliente.java
+++ src/main/java/br/edu/univille/salaobeleza/entity/Cliente.java
@@ -9,15 +9,15 @@ import jakarta.persistence.Id;
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String nome;
     private String telefone;
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
---
+++ src/main/java/br/edu/univille/salaobeleza/entity/Produto.java
@@ -0,0 +1,50 @@
+package br.edu.univille.salaobeleza.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private double preco;
+    private String descricao;
+    private String tipo;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
+
+
