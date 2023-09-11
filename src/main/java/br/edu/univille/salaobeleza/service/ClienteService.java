3d4fd674876cc393fa9f9a7fe836e599ed2c4ea8
asschmoeller <asschmoeller@gmail.com>
2023-09-11 20:37:22 -0300

    novas atualizações

---
+++ src/main/java/br/edu/univille/salaobeleza/service/ClienteService.java
@@ -0,0 +1,10 @@
+package br.edu.univille.salaobeleza.service;
+
+import java.util.List;
+
+import br.edu.univille.salaobeleza.entity.Cliente;
+
+public interface ClienteService {
+    List<Cliente> getAll();
+    
+}
