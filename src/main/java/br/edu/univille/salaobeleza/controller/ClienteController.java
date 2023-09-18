c8d19dc9de3becfce3410043710fddfbb5304571
ANDRESA SIEDSCHLAG SCHMÖLLER <andresa.siedschlag@univille.br>
2023-09-18 19:26:18 -0300

    aula 11-08

--- src/main/java/br/edu/univille/salaobeleza/controller/ClienteController.java
+++ src/main/java/br/edu/univille/salaobeleza/controller/ClienteController.java
@@ -1,16 +1,31 @@
 package br.edu.univille.salaobeleza.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.edu.univille.salaobeleza.Repository.clienteRepository;
+import br.edu.univille.salaobeleza.entity.Cliente;
+import br.edu.univille.salaobeleza.service.ClienteService;
+
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
 
+
+    @Autowired
+    private ClienteService service;
+
     @GetMapping
     public ModelAndView index(){
-        return new ModelAndView("cliente/index");
+
+
+        var listaClientes = service.getAll();
+        
+        return new ModelAndView("cliente/index", "listaClientes", listaClientes);
     }
 }
--- src/main/java/br/edu/univille/salaobeleza/service/impl/ClienteServiceImpl.java
+++ src/main/java/br/edu/univille/salaobeleza/service/impl/ClienteServiceImpl.java
@@ -2,15 +2,19 @@ package br.edu.univille.salaobeleza.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.edu.univille.salaobeleza.Repository.clienteRepository;
 import br.edu.univille.salaobeleza.entity.Cliente;
 import br.edu.univille.salaobeleza.service.ClienteService;
 
 public class ClienteServiceImpl implements ClienteService{
 
+    @Autowired
+    private clienteRepository repository;
     @Override
     public List<Cliente> getAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+        return repository.findAll();
     }
     
 }
